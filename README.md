fe3f54df0a71660a268ef09487dc1c62aed39c1c
Dinah Chen <dinah.chen.ccy@gmail.com>
2019-11-26 16:29:25 -0800

    v1.0

---
+++ README.md
@@ -0,0 +1,3 @@
+# Project Description
+## A simple contact us form
+A button that loads a form, and upon form submission, a confirmation/thank you page is triggered to enable conversion tracking. The form fields are Name, Company, Phone Number, Email, What Are You Looking For?, and a space to upload pdf or jpg files. A reCAPTCHA anti-spam library is added to protect submissions, too.
---
+++ inc/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Demo | Contact Form</title>
+    <link rel="icon" type="image/png" href="img/favicon.png" />
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.4.1.min.js"></script>
+    <script src="js/custom.js"></script>
+</head>
+<body>
+    
+</body>
+</html>
